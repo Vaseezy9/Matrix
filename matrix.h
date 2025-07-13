01c3a854648f0421f3076dd1ae39b5b1bf2fc2b4
Vaseezy9 <139249278+Vaseezy9@users.noreply.github.com>
2025-07-13 23:20:29 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -32,21 +32,21 @@ namespace math {
             }
 
             Matrix& operator+=(const Matrix& other) {
-                if (rows_ != other rows_ || cols_ != other cols_) {
+                if (rows_ != other.rows_ || cols_ != other.cols_) {
                     return *this;
                 }
                 for (int i = 0; i < rows_ * cols_; ++i) {
-                    mvec_[i] += other mvec_[i];
+                    mvec_[i] += other.mvec_[i];
                 }
                 return *this;
             }
 
             Matrix& operator-=(const Matrix& other) {
-                if (rows_ != other rows_ || cols_ != other cols_) {
+                if (rows_ != other.rows_ || cols_ != other.cols_) {
                     return *this;
                 }
                 for (int i = 0; i < rows_ * cols_; ++i) {
-                    mvec_[i] -= other mvec_[i];
+                    mvec_[i] -= other.mvec_[i];
                 }
                 return *this;
             }
@@ -59,7 +59,7 @@ namespace math {
             }
 
             friend std::ostream& operator<<(std::iostream& os, const Matrix& matrix) {
-                matrix print();
+                matrix.print(os);
                 return os;
             }
 
@@ -77,33 +77,33 @@ namespace math {
             friend Matrix operator*(const Matrix& A, const Matrix& B);
     };
 
-    math::Matrix operator+(const math::Matrix& A, const math::Matrix& B) {
+    Matrix operator+(const Matrix& A, const Matrix& B) {
         if (A rows_ != B rows_ || A cols_ != B cols_) {
-            return math::Matrix();
+            return Matrix();
         }
-        math::Matrix result(A rows_, A cols_);
+        Matrix result(A rows_, A cols_);
         for (int i = 0; i < A rows_ * A cols_; ++i) {
             result mvec_[i] = A mvec_[i] + B mvec_[i];
         }
         return result;
     }
 
-    math::Matrix operator-(const math::Matrix& A, const math::Matrix& B) {
+    Matrix operator-(const Matrix& A, const Matrix& B) {
         if (A rows_ != B rows_ || A cols_ != B cols_) {
-            return math::Matrix();
+            return Matrix();
         }
-        math::Matrix result(A rows_, A cols_);
+        Matrix result(A rows_, A cols_);
         for (int i = 0; i < A rows_ * A cols_; ++i) {
             result mvec_[i] = A mvec_[i] - B mvec_[i];
         }
         return result;
     }
 
-    math::Matrix operator*(const math::Matrix& A, const math::Matrix& B) {
+    Matrix operator*(const Matrix& A, const Matrix& B) {
         if (A cols_ != B rows_) {
-            return math::Matrix();
+            return Matrix();
         }
-        math::Matrix result(A rows_, B cols_);
+        Matrix result(A rows_, B cols_);
         for (int i = 0; i < A rows_; ++i) {
             for (int j = 0; j < B cols_; ++j) {
                 real sum = 0;
